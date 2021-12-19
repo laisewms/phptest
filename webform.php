776d69757127ce2efd7f7b6a0c4b1ea75081c49d
Laise <55756285+laisewms@users.noreply.github.com>
2021-12-19 15:04:27 -0500

    Add files via upload

---
+++ webform.php
@@ -0,0 +1,189 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <title>Welita Cleaners</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/png" href="images/hero-content/web-logo.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Shippori+Antique&display=swap" rel="stylesheet"> 
+    <script src="https://kit.fontawesome.com/c9642beef6.js" crossorigin="anonymous"></script>
+</head>
+
+<div class="navbar-area">
+    <div class="welitas-nav-top-bar d-none d-lg-block">
+      <div class="container-top-bar">
+        <div class="row">
+          <div class="col-lg-8 col-md-6">
+            <div class="call-to-action">
+              <p><i class="fas fa-map-marker-alt"></i>
+              Pittsburgh
+              </p>
+              <p>
+                <i class="fas fa-envelope"></i>
+                <a href="mailto:welitaluizaar@gmail.com"><span class="email-color">welitaluizaar@gmail.com</span>
+                </a>
+              </p>
+              <p>
+                <i class="fas fa-mobile-alt"></i>
+                <a href="tel:17245538208"><span class="num-color">+1 724 553 8208</span></a>
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <ul class="top-social">
+              <li>
+                <a href="https://www.facebook.com/welita_cleaners"target="_blank"><i class="fab fa-facebook-f fa-2x"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/welitacleaners/"target="_blank"><i class="fab fa-instagram fa-2x"></i>
+                </a>
+              </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  <!--NAV BAR-->
+  <nav class="navbar" id="jump-home">
+    <div class="container-nav">
+      <a href="#jump-home"><img class="logo" src="/images/hero-content/web-logo.png" alt="logo"/></a>
+      <h3 class="logo-text"><a href="#jump-home">Welita Cleaners</a></h3>
+    </div>
+    <a href="#" id="toggle-button" class="toggle-button">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+    </a>
+    <div class="nav-links">
+        <ul>
+          <li class="nav-items"><a href="#jump-home">Home</a></li>
+          <li class="nav-items"><a href="#jump-services">Services</a></li>
+          <li class="nav-items"><a href="#jump-about">About Us</a></li>
+          <li class="nav-items"><a href="#jump-contact">Contact</a></li>
+        </ul>
+    </div>
+</nav>
+<section class="uc-hero-contact">
+    <div class="container-md uc-hero-text">
+      <h1>Contact</h1>
+    </div>
+  </section>
+
+  <main class="container-md uc-container-main">
+
+    <section class="uc-contact-left">
+      <h2>General enquiries</h2>
+      <p>Have a question about wines in Canberra? Our team is more than happy to help you out.</p>
+    </section>
+
+    <section class="uc-contact-right">
+      <form class="needs-validation" novalidate action="https://formsubmit.co/c41015b0f8d2e08475a9720e56de574f" method="POST">
+        <!--Honeypot-->
+        <input type="text" name="_honey" style="display: none;">
+        <!--Disable captcha-->
+        <input type="hidden"name="_captcha" value="false">
+        <div class="form-group">
+          <label for="ucContactName">Name</label>
+          <input type="text" class="form-control" id="ucContactName" placeholder="Name" required>
+          <div class="valid-feedback">Looks great!</div>
+          <div class="invalid-feedback">Please fill out this field.</div>
+        </div>
+        <div class="form-group">
+          <label for="ucContactEmail">Email address</label>
+          <input type="email" class="form-control" id="ucContactEmail" placeholder="name@email.com" required>
+          <div class="valid-feedback">Looks great!</div>
+          <div class="invalid-feedback">Please use a valid email address.</div>
+        </div>
+        <div class="form-group">
+          <label for="ucContactMessage">Message</label>
+          <textarea placeholder="Let us know how we can help you today!" class="form-control" id="ucContactMessage" rows="3" required></textarea>
+          <div class="valid-feedback">Looks great!</div>
+          <div class="invalid-feedback">Please fill out this field.</div>
+        </div>
+        <button type="submit" class="uc-btn">Send your message</button>
+      </form>
+    </section>
+  </main>
+</section>
+<!--Footer-->
+  <button id="back-to-top-btn"><i class="fas fa-angle-double-up"></i></button>
+
+    <footer class="footer-section">
+      <div class="container">
+        <div class="footer-cta pt-5 pb-5">
+          <div class="row">
+            <div class="col-xl-4 col-md-4 mb-30">
+              <div class="single-cta">
+                <i class="fas fa-map-marker-alt"></i>
+                <div class="cta-text">
+                  <h4>Pittsburgh</h4>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-4 col-md-4 mb-30">
+              <div class="single-cta">
+                <i class="fas fa-phone"></i>
+                <div class="cta-text">
+                  <h4>Call us</h4>
+                    <a href="tel:17245538208"><span class="num-color">+1 724 553 8208</span></a>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-4 col-md-4 mb-30">
+              <div class="single-cta">
+                <i class="far fa-envelope-open"></i>
+                <div class="cta-text">
+                  <h4>Mail us</h4>
+                    <a href="mailto:welitaluizaar@gmail.com"><span class="email-color">welitaluizaar@gmail.com</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="footer-content pt-5 pb-5">
+          <div class="row">
+            <div class="col-xl-4 col-lg-4 mb-50">
+              <div class="footer-widget">
+                <div class="footer-social-icon">
+                  <h3>Follow us</h3>
+                  <a href="https://www.facebook.com/welita_cleaners"target="_blank"><i class="fab fa-facebook-f facebook-bg"></i></a>
+                  <a href="https://www.instagram.com/welitacleaners/"target="_blank"><i class="fab fa-instagram twitter-bg"></i></a>
+                </div>
+              </div>
+          </div>
+          <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+          <div class="footer-widget">
+            <ul>
+              <li><a href="#jump-home">Home</a></li>
+              <li><a href="#jump-services">Services</a></li>
+              <li><a href="#jump-about">About Us</a></li>
+              <li><a href="contact.html">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+</div>
+<div class="copyright-area">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-6 col-lg-6 text-center text-lg-left">
+          <div class="copyright-text">
+            <p>Copyright &copy; 2021, Welita Cleaners.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </footer>
+
